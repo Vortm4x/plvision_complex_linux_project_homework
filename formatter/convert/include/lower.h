d412303fb42159ba5dca7ff2137ae9b753627c2c
Maksym Trehubenko <maks.trehubenko@gmail.com>
2025-11-07 00:23:00 +0100

    Add convert dyn lib project skeleton
    Signed-off-by: Maksym Trehubenko <maks.trehubenko@gmail.com>

---
+++ formatter/convert/include/lower.h
@@ -0,0 +1,4 @@
+#ifndef _CONVERT_LOWER_H_
+#define _CONVERT_LOWER_H_
+
+#endif // _CONVERT_LOWER_H_
---
+++ formatter/convert/include/upper.h
@@ -0,0 +1,4 @@
+#ifndef _CONVERT_UPPER_H_
+#define _CONVERT_UPPER_H_
+
+#endif // _CONVERT_UPPER_H_
